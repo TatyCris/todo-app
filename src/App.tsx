a2ec58b9649eac4323571dc6a3abfa29f15b42bf
taty <t.costa@viriciti.com>
2025-10-17 12:12:52 +0200

    add Inter font from google fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-	return <h1 className="text-4xl">Test</h1>;
+	return (
+		<div className="font-sans">
+			<h1 className="text-4xl">Test</h1>;
+		</div>
+	);
 }
 
 export default App;
