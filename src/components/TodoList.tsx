61831be3a4c97b7be901fcbe2824deb2d32e36fc
taty <t.costa@viriciti.com>
2025-10-17 20:04:34 +0200

    create TodoList

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,3 +1,18 @@
+import DeleteButton from "./DeleteButton";
+
+const initialTodos = ["buy groceries", "walk the dog", "do laudry"];
 export default function TodoList() {
-	return <ul></ul>;
+	return (
+		<ul>
+			{initialTodos.map((todo) => (
+				<li
+					key={todo}
+					className="flex justify-between items-center border-b border-black/[8%] h-[50px] px-8 cursor-pointer"
+				>
+					<span>{todo}</span>
+					<DeleteButton />
+				</li>
+			))}
+		</ul>
+	);
 }
