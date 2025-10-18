6f1b6b08b7bac9d921fc7ff9ed32af833317344c
taty <t.costa@viriciti.com>
2025-10-18 11:41:48 +0200

    delete and complete todos

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -1,3 +1,12 @@
-export default function DeleteButton() {
-	return <button>❌</button>;
+export default function DeleteButton({ setTodos, id }) {
+	return (
+		<button
+			onClick={(e) => {
+				e.stopPropagation();
+				setTodos((prev) => prev.filter((todo) => todo.id !== id));
+			}}
+		>
+			❌
+		</button>
+	);
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,16 +1,45 @@
+import { useState } from "react";
 import DeleteButton from "./DeleteButton";
 
-const initialTodos = ["buy groceries", "walk the dog", "do laudry"];
 export default function TodoList() {
+	const [todos, setTodos] = useState([
+		{
+			id: 1,
+			text: "buy groceries",
+			completed: false,
+		},
+		{
+			id: 2,
+			text: "walk the dog",
+			completed: true,
+		},
+		{
+			id: 3,
+			text: "do laudry",
+			completed: false,
+		},
+	]);
+
 	return (
 		<ul>
-			{initialTodos.map((todo) => (
+			{todos.map((todo) => (
 				<li
-					key={todo}
+					key={todo.id}
 					className="flex justify-between items-center border-b border-black/[8%] h-[50px] px-8 cursor-pointer"
+					onClick={() => {
+						setTodos(
+							todos.map((t) =>
+								t.id === todo.id ? { ...t, completed: !t.completed } : t
+							)
+						);
+					}}
 				>
-					<span>{todo}</span>
-					<DeleteButton />
+					<span
+						className={`${todo.completed ? "line-through text-[#ccc]" : ""}`}
+					>
+						{todo.text}
+					</span>
+					<DeleteButton id={todo.id} setTodos={setTodos} />
 				</li>
 			))}
 		</ul>
